3ff80558f5f59f1401ada59514a98a8fd5f8b296
banabda <banabda@gmail.com>
2020-04-19 09:17:11 +0700

    shared folder

---
+++ lib/shared/constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+var textInputDecoration = InputDecoration(
+  border: InputBorder.none,
+  hintText: "Password",
+  hintStyle: TextStyle(color: Colors.grey[400]),
+);
+
+var boxInputDecoration = BoxDecoration(
+  border: Border(
+    bottom: BorderSide(
+      color: Colors.grey[100],
+      )
+      )
+);
